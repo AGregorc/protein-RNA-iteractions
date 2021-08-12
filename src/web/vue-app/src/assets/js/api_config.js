3df93a5f294628fd19944a439093fa88228cfbbf
AGregorc <anze.gregorc44@gmail.com>
2021-08-12 14:35:50 +0200

    Fix 3Dmol lib - add script instead of url, fix vue env variable

--- src/web/vue-app/src/assets/js/api_config.js
+++ src/web/vue-app/src/assets/js/api_config.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-let URL = process.env.API_URL || 'http://' + window.location.hostname + ':5004/api/';
+let URL = process.env.VUE_APP_API_URL || 'http://' + window.location.hostname + ':5004/api/';
 
 let GetPredictionsURL = URL + 'get_predictions/';
 let ListAllPdbsURL = URL + 'list_all_pdbs';
